26c789488016d887b02d52d99c557baf8074bb1a
Mike <mihansp1@gmail.com>
2019-10-20 20:12:52 +0300

    Intial task-3 commit

---
+++ TaskSolving/Task3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Разберитесь с инкапсуляцией
+namespace TaskSolving
+{
+    class Bag
+    {
+        public List<Item> Items;
+        public int MaxWeidth;
+
+        public void AddItem(string name, int count)
+        {
+            int currentWeidth = Items.Sum(item => item.Count);
+            Item targetItem = Items.FirstOrDefault(item => item.Name == name);
+
+            if (targetItem == null)
+                throw new InvalidOperationException();
+
+            if (currentWeidth + count > MaxWeidth)
+                throw new InvalidOperationException();
+
+            targetItem.Count += count;
+        }
+    }
+
+    class Item
+    {
+        public int Count;
+        public string Name;
+    }
+}
