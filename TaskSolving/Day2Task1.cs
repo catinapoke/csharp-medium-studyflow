51c5a01c8c2a962aee9f546feaf2021edd8fa533
Mike <mihansp1@gmail.com>
2019-10-27 11:39:09 +0300

    Initial task condition

---
+++ TaskSolving/Day2Task1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TaskSolving
+{
+    class Wombat
+    {
+        public int Health;
+        public int Armor;
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage - Armor;
+            if (Health <= 0)
+            {
+                Console.WriteLine("Я умер");
+            }
+        }
+    }
+
+    class Human
+    {
+        public int Health;
+        public int Agility;
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage / Agility;
+            if (Health <= 0)
+            {
+                Console.WriteLine("Я умер");
+            }
+        }
+    }
+}
