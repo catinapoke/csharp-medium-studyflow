c6944c3fe68eaf47b886521eaf3f011009832b75
Mike <mihansp1@gmail.com>
2019-10-27 11:56:00 +0300

    Used abstract class, so removed duplicated code

--- TaskSolving/Day2Task1.cs
+++ TaskSolving/Day2Task1.cs
@@ -2,33 +2,47 @@
 
 namespace TaskSolving
 {
-    class Wombat
+    abstract class Damagable
     {
-        public int Health;
-        public int Armor;
-
-        public void TakeDamage(int damage)
+        protected int _health;
+        public int Health { get => _health; private set => _health = value; }
+        public void TakeDamage(int rawDamage)
         {
-            Health -= damage - Armor;
-            if (Health <= 0)
+            _health -= DamageCalculate(rawDamage);
+            DeadCheck();
+        }
+        protected void DeadCheck()
+        {
+            if (_health <= 0)
             {
                 Console.WriteLine("Я умер");
             }
         }
+        protected virtual int DamageCalculate(int rawDamage)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    class Human
+    class Wombat : Damagable
     {
-        public int Health;
-        public int Agility;
+        private int _armor;
+        public int Armor { get => _armor; private set => _armor = value; }
 
-        public void TakeDamage(int damage)
+        protected override int DamageCalculate(int rawDamage)
         {
-            Health -= damage / Agility;
-            if (Health <= 0)
-            {
-                Console.WriteLine("Я умер");
-            }
+            return rawDamage - _armor;
+        }
+    }
+
+    class Human : Damagable
+    {
+        private int _agility;
+        public int Agility { get => _agility; private set => _agility = value; }
+
+        protected override int DamageCalculate(int rawDamage)
+        {
+            return rawDamage / _agility;
         }
     }
 }
